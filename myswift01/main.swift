36373a4e0a408174a76ffc8a30d3c9d4fb13d69c
user4 <user4@user4demacbook-pro.local>
2017-09-06 10:54:23 +0800

    Initial Commit

---
+++ myswift01/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  myswift01
+//
+//  Created by user4 on 2017/9/6.
+//  Copyright © 2017年 user4wangwangwang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print(4+5)
+var a = Int.max;
+print(a);
+var b = a-1;
+print(b)
+
+
+
+
